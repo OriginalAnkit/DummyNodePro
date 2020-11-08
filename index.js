128be20528154eb863882db9bd13e0eb156e98e3
Ankit <akt.halder@gmail.com>
2020-11-08 17:20:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+const { networkInterfaces } = require('os');
+
+//create a server object:
+http.createServer(function (req, res) {
+   let data=networkInterfaces();
+   let obj={
+       processId:process.pid,
+       ip:data.wlp2s0
+   }
+   res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(obj))
+}).listen(8080); 
